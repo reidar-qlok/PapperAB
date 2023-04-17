5ba19ca13c3bcadcb7c49e63e3414318625d3451
reidar-qlok <reidar-nilsen@qlok.se>
2023-04-17 16:03:01 +0200

    Day 2

--- PapperAB/Models/Employee.cs
+++ PapperAB/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace PapperAB.Models
 {
@@ -10,9 +11,11 @@ namespace PapperAB.Models
         public int EmployeeId { get; set; } = 0;
         [Required]
         [StringLength(50)]
+        [DisplayName("First name")]
         public string FirstMidName { get; set; } = default!;
         [Required]
         [StringLength(30)]
+        [DisplayName("Last name")]
         public string LastName { get; set; } = default!;
         [StringLength(25)]
         public string Address { get; set; } = default!;
--- PapperAB/Models/ProjectList.cs
+++ PapperAB/Models/ProjectList.cs
@@ -14,9 +14,9 @@ namespace PapperAB.Models
         public DateTime Stop { get; set; }
         [ForeignKey("Employees")]
         public int FK_EmployeeId { get; set; }
-        public virtual Employee Employees { get; set; }
+        public virtual Employee Employees { get; set; }//Navigering
         [ForeignKey("Projects")]
         public int FK_ProjectId { get; set; }
-        public virtual Project Projects { get; set; }
+        public virtual Project Projects { get; set; }//navigering
     }
 }
