d9a0e53ce7b938e848adfa3ddf5417018f3cef1d
reidar-qlok <reidar-nilsen@qlok.se>
2023-04-19 08:49:11 +0200

    Versionen från 2023-04-18

---
+++ PapperAB/Controllers/InfoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PapperAB.Data;
+using PapperAB.Models;
+
+namespace PapperAB.Controllers
+{
+    public class InfoController : Controller
+    {
+        private readonly PapperABContext context;
+        public InfoController(PapperABContext _context)
+        {
+            context = _context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<InfoViewModel> list = new List<InfoViewModel>();
+            var items = await (from emp in context.Employees
+                               join pl in context.ProjectLists on emp.EmployeeId equals pl.FK_EmployeeId
+                               join proj in context.Projects on pl.FK_ProjectId equals proj.ProjectId
+                               select new
+                               {
+                                   Start = pl.Start,
+                                   Stop = pl.Stop,
+                                   ProjectName= proj.ProjectName,
+                                   Description = proj.Description,
+                                   FirstMidName = emp.FirstMidName,
+                                   LastName=emp.LastName
+                               }).ToListAsync();
+            // konvertera från anonymous
+            foreach (var item in items)
+            {
+                InfoViewModel listItem = new InfoViewModel();
+                listItem.Start = item.Start;
+                listItem.Stop = item.Stop;
+                listItem.ProjectName = item.ProjectName;
+                listItem.Description = item.Description;
+                listItem.FirstMidName = item.FirstMidName;
+                listItem.LastName = item.LastName;
+                list.Add(listItem);
+            }
+            // Kalla på view
+            return View(list);
+        }
+    }
+}
---
+++ PapperAB/Models/InfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PapperAB.Models
+{
+    public class InfoViewModel
+    {
+        public string FirstMidName { get; set; }
+        public string LastName { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Stop { get; set; }
+    }
+}
